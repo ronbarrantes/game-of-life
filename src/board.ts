0c6008d1d1961ff0b501cf0f9c7901ddb90c4cf2
ronbarrantes <ronbarrantes@gmail.com>
2020-04-19 10:36:19 -0700

    Create cells

--- src/board.ts
+++ src/board.ts
@@ -1,39 +1,46 @@
 const board = document.createElement('canvas')
 board.className = 'board'
+const boardSize = '300'
+
+const state = [
+  [1, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0],
+]
 
 const resizeBoard = (): void => {
-  const boardSize = '300'
   const windowMid = window.innerWidth/2
   const boardMid = +boardSize/2
   const midPoint = `${windowMid-boardMid}px`
   board.setAttribute('width', boardSize)
   board.setAttribute('height', boardSize)
   board.style.marginLeft = midPoint
-  console.log(window.innerWidth)
 }
 
 resizeBoard()
-
 window.onresize = resizeBoard
 
 const ctx = board.getContext('2d')
 
+const cell = (x: number, y: number, w: number, h: number, alive?: number|boolean): void => {
+  ctx.fillStyle = alive ? 'black' : 'white'
+  ctx.fillRect(x, y, w, h)
+}
+
 const draw = (): void => {
+  // TODO: draw the board
+  cell(0, 0, 50, 50, 1)
+  cell(50, 0, 50, 50, 1)
+  cell(100, 0, 50, 50, 1)
   window.requestAnimationFrame(draw)
-  ctx.fillStyle = 'green'
-  ctx.fillRect(0, 0, 100, 100)
-
-  ctx.fillStyle = 'red'
-  ctx.fillRect(100, 0, 100, 100)
-
-  ctx.fillStyle = 'blue'
-  ctx.fillRect(200, 0, 100, 100)
 }
 
 const init = (): void => {
   window.requestAnimationFrame(draw)
 }
-
 init()
 
 export default board
